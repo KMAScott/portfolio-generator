29803efb47636e3590f5dff6fd3384c5d8e3893e
Scott Ashley <burnbright987@gmail.com>
2022-05-25 12:47:38 -0400

    updated HTML template

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
-
-//const pageHTML = generatePage(name, github);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-//fs.writeFile('index.html', pageHTML, err => {
-    //if (err) throw err;
-
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -137,5 +130,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+        console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
